1bdc1ebb170a2ae10ed2c7922ab45b1b45c3a4ca
hamzahassan0923 <hamzahassan0923@gmail.com>
2026-06-28 16:21:19 +0600

    add

--- src/lib/api/opportunity.js
+++ src/lib/api/opportunity.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://startup-forge-server.vercel.app";
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 export const getOpportunities = async (companyId, status='active') => {
     const res = await fetch(`${baseUrl}/api/opportunities?companyId=${companyId}&status=${status}`);
     return res.json();
